b02ce8ece10fcea6a73142015515ee2c6bd74ff7
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-09-30 14:36:47 +0530

    database connected

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import dotenv from 'dotenv'
+
+// Import Routes
+import dashboardRoutes from  './routes/dashboardRoutes'
+
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));  
+
+// Routes
+
+app.use("/dashboard", dashboardRoutes)
+
+// Server
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+})
